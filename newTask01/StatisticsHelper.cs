83bae788b07366fbed2e9e3abc46931c8a1f5bb2
Alex Kozhar <garu1996@example.com>
2026-03-16 15:27:54 +0100

    Implement CalculateMin logic

--- newTask01/StatisticsHelper.cs
+++ newTask01/StatisticsHelper.cs
@@ -6,24 +6,32 @@ public class StatisticsHelper
     {
         return (a + b) / 2.0;
     }
+
     public static double CalculateAverage(int[] values)
     {
-        if(values.Length == 0) return 0;
+        if (values.Length == 0) return 0;
         double sum = 0;
-        foreach(var v in values) sum += v;
+        foreach (var v in values) sum += v;
         return sum / values.Length;
     }
+
     public static int CalculateMax(int[] values)
     {
-        if(values.Length == 0) return int.MinValue;
+        if (values.Length == 0) return int.MinValue;
         int max = values[0];
-        foreach(var v in values)
-            if(v > max) max = v;
+        foreach (var v in values)
+            if (v > max)
+                max = v;
         return max;
         //TO Expand The Logic
     }
+
     public static int CalculateMin(int[] values)
     {
-        //TODO
+        if(values.Length == 0) return int.MaxValue;
+        int min = values[0];
+        foreach(var v in values)
+            if(v < min) min = v;
+        return min;
     }
 }
